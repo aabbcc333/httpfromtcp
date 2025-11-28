a4dd2ebf4fe83f3be3a681007f6e123b747db75e
aabbcc333 <pooniaraunak@gmail.com>
2025-11-28 17:20:49 +0530

    header, multiple values

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -21,9 +21,26 @@ assert.False(t, done)
 
 // Test: Invalid spacing header
 headers = NewHeaders()
+data = []byte("       Host : localhost:42069       \r\n\r\n")
+n, done, err = headers.Parse(data)
+require.Error(t, err)
+assert.Equal(t, 0, n)
+assert.False(t, done)
+
+headers = NewHeaders()
 data = []byte("H©st: localhost:42069\r\n\r\n")
 n, done, err = headers.Parse(data)
 require.Error(t, err)
 assert.Equal(t, 0, n)
 assert.False(t, done)
+
+headers = NewHeaders()
+data = []byte("Host: localhost:42069\r\nHost: localhost:42069\r\n")
+n, done, err = headers.Parse(data)
+require.NoError(t, err)
+require.NotNil(t, headers)
+assert.Equal(t, "localhost:42069,localhost:42069", headers.Get("HOST")) 
+assert.False(t,done)
 }
+
+
