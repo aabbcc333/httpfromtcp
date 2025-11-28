783463022d42f208ba20aa855368dc58ab877c6f
aabbcc333 <pooniaraunak@gmail.com>
2025-11-28 18:16:48 +0530

    adding live headers, adding headers as key value pairs in console

--- cmd/tcplistner/main.go
+++ cmd/tcplistner/main.go
@@ -1,72 +1,43 @@
 package main
 
 import (
-	//"bytes"
-	"bytes"
-	"fmt"
-	"io"
-	"log"
-	"net"
-	//"os"
+    "fmt"
+    "log"
+    "net"
+
+    "github.com/aabbcc333/httpfromtcp/internal/request"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string{
-	out := make(chan string,1)
-	buffer := make([]byte, 8)
-	var str []byte
-	go func(){
-		defer f.Close()
-		defer close(out)
-		for{
-			n,err := f.Read(buffer)
-			
-
-			chunk := buffer[:n]
-			for len(chunk)>0{
-             index := bytes.IndexByte(chunk,'\n')
-			 if index == -1{
-				str = append(str, chunk...)
-				chunk = chunk[:0]
-				continue
-			 }
-			 strPart := chunk[:index]
-			 str = append(str,strPart...)
-			 out <- string(str)
-			 str = str[:0]
-			 chunk = chunk[index+1:]
-			}
-
-			if err == io.EOF{
-				if len(str)>0{
-					out <- string(str)
-				}
-				return
-			}
-			if err != nil {
-				fmt.Printf("error %s", err)
-				return
-			}
-		}
-		
-	
-	}()
-	return out
-}
-
 func main() {
-  listner, err := net.Listen("tcp",":42069")
-  if err != nil {
-	log.Fatal("error","error",err)
-  }
+    listener, err := net.Listen("tcp", ":42069")
+    if err != nil {
+        log.Fatal("error", err)
+    }
 
-  for{
-	conn,err := listner.Accept()
-	if err != nil{
-		
-	log.Fatal("error","error",err)
-	}
-	for line := range getLinesChannel(conn){
-		fmt.Printf("read:%s\n",line)
-	}
-  }
+    log.Println("listening on :42069")
+
+    for {
+        log.Println("listening on :42069 entering loop")
+        conn, err := listener.Accept()
+        if err != nil {
+            log.Fatal("accept error:", err)
+        }
+
+        log.Println("accepted connection from", conn.RemoteAddr())
+
+        r, err := request.RequestFromReader(conn)
+        if err != nil {
+            log.Fatal("request parse error:", err)
+        }
+
+        fmt.Printf("Request line:\n")
+        fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+        fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+        fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
+
+        fmt.Printf("Headers:\n")
+        r.Headers.ForEach(func(n, v string) {
+            fmt.Printf("- %s: %s\n", n, v)
+        })
+    }
 }
--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,94 +1,99 @@
 package headers
 
 import (
-	"bytes"
-	"fmt"
-	"strings"
+    "bytes"
+    "fmt"
+    "strings"
 )
 
-func isToken(str []byte) bool{
+func isToken(str []byte) bool {
 
-	for _, ch := range str{
-		found := false 
-		if ch >= 'A' && ch <='Z' ||
-		  ch >= 'a' && ch <= 'z'||
-		  ch >= '0' &&  ch <= '9'{
-			found = true 
-		  }
-		switch ch{
-		case '#','$','%','&','\'','*','+','-','.','^','_','`','|','~':
-		    found = true
-		}
-		if !found{
-			return false
-		}
-	
-	}
-	return true
+    for _, ch := range str {
+        found := false
+        if ch >= 'A' && ch <= 'Z' ||
+            ch >= 'a' && ch <= 'z' ||
+            ch >= '0' && ch <= '9' {
+            found = true
+        }
+        switch ch {
+        case '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+            found = true
+        }
+        if !found {
+            return false
+        }
+
+    }
+    return true
 
 }
 
 type Headers struct {
-	headers map[string]string
+    headers map[string]string
 }
 
 var rn = []byte("\r\n")
 
-func parseHeader(filedLine []byte)(string, string, error){
-	parts := bytes.SplitN(filedLine, []byte(":"),2)
-	if len(parts) != 2{
-	   return "", "", fmt.Errorf("malfromed header")
-	}
-	name := parts[0]
-	value := bytes.TrimSpace(parts[1])
-	if bytes.HasSuffix(name, []byte(" ")){
-	   return "","",fmt.Errorf("maleformed filed name") 
-	}
-	return string(name), string(value) , nil
+func parseHeader(filedLine []byte) (string, string, error) {
+    parts := bytes.SplitN(filedLine, []byte(":"), 2)
+    if len(parts) != 2 {
+        return "", "", fmt.Errorf("malfromed header")
+    }
+    name := parts[0]
+    value := bytes.TrimSpace(parts[1])
+    if bytes.HasSuffix(name, []byte(" ")) {
+        return "", "", fmt.Errorf("maleformed filed name")
+    }
+    return string(name), string(value), nil
 }
 
-func NewHeaders() *Headers{
-	return &Headers{
-       headers : map[string]string{},
-	}
+func NewHeaders() *Headers {
+    return &Headers{
+        headers: map[string]string{},
+    }
 }
 
-func(h *Headers) Get(name string) string{
-	return h.headers[strings.ToLower(name)]
+func (h *Headers) Get(name string) string {
+    return h.headers[strings.ToLower(name)]
 }
 
 func (h *Headers) Set(name, value string) {
-	name = strings.ToLower(name)
+    name = strings.ToLower(name)
 
-	if v, ok := h.headers[name]; ok{
-		h.headers[name] = fmt.Sprintf("%s,%s", v, value)
-	}else{
-		h.headers[name] = value
-	}
-	
+    if v, ok := h.headers[name]; ok {
+        h.headers[name] = fmt.Sprintf("%s,%s", v, value)
+    } else {
+        h.headers[name] = value
+    }
+
 }
-func (h Headers) Parse(data []byte) (int, bool, error){
-	read := 0 
-	done := false
-	for{
-		idx := bytes.Index(data[read:], rn)
-		if idx == -1{
-			break 
-		}
-		if idx == 0{
-			done = true 
-			read += len(rn)
-			break
-		}
-		name, value, err := parseHeader(data[read:read+idx])
-		if err != nil{
-			return 0, false, err
-		}
-		if !isToken([]byte(name)){
-			 return 0, false, fmt.Errorf("malformed header name")
-		}
-		read += idx + len(rn)   
-		h.Set(name, value)
-	}
-	return read,done,nil
+func (h *Headers) ForEach(cb func(n, v string)) {
+    for n, v := range h.headers {
+        cb(n, v)
+    }
+}
+func (h *Headers) Parse(data []byte) (int, bool, error) {
+    read := 0
+    done := false
+    for {
+        idx := bytes.Index(data[read:], rn)
+        if idx == -1 {
+            break
+        }
+        if idx == 0 {
+            done = true
+            read += len(rn)
+            break
+        }
+        name, value, err := parseHeader(data[read : read+idx])
+        if err != nil {
+            return 0, false, err
+        }
+        if !isToken([]byte(name)) {
+            return 0, false, fmt.Errorf("malformed header name")
+        }
+        read += idx + len(rn)
+        h.Set(name, value)
+    }
+    return read, done, nil
 }
